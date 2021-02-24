706012ebb556ab8bf29e9f01036b046564d1ca82
VinsonWang92 <vinsonwang92@gmail.com>
2021-02-24 16:14:51 +1100

    added seed data in keystone

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -5,6 +5,7 @@ import { createAuth } from '@keystone-next/auth';
 import {withItemData, statelessSessions } from '@keystone-next/keystone/session';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage'
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -33,7 +34,11 @@ export default withAuth(config({
     db: {
         adapter: 'mongoose',
         url: databaseURL,
-        // TODO: add data seeding here
+        async onConnect(keystone) {
+            if (process.argv.includes('--seed-data')) {
+            await insertSeedData(keystone);
+            }
+        },
     },
     lists: createSchema({
         //Schema items go in here
