28250f60e39c3786922e0ab5a69be9de177659c5
VinsonWang92 <vinsonwang92@gmail.com>
2021-02-25 18:39:49 +1100

    fetched data using graphQL hook, added css to displayed items

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -10,7 +10,6 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({Component, pageProps, apollo}) {
-    console.log(apollo)
     return (
     <ApolloProvider client={apollo}>
         <Page>
@@ -23,7 +22,7 @@ function MyApp({Component, pageProps, apollo}) {
 MyApp.getInitialProps = async function({ Component, ctx }) {
     let pageProps = {};
     if(Component.getInitialProps) {
-        pageProps = await Component.getIntiialProps(ctx);
+        pageProps = await Component.getInitialProps(ctx);
     }
     pageProps.query = ctx.query;
     return {pageProps}
